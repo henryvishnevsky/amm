0df9cdc3965cbe0e31188747e675f59e3d3f7434
henryvishnevsky <github.hn9cy@passmail.com>
2024-01-11 11:00:47 -0700

    Added Remove Liquidity function and tests

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -1,13 +1,8 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "./Token.sol";
-
-// [] Manage Pool
-// [] Manage Deposit
-// [] Facilitates Swaps (Trades)
-// [] Manage Withdrawals
 
 contract AMM {
     Token public token1;
@@ -30,7 +25,6 @@ contract AMM {
         uint256 token1Balance,
         uint256 token2Balance,
         uint256 timestamp
-
     );
 
     constructor(Token _token1, Token _token2) {
@@ -42,57 +36,55 @@ contract AMM {
         // Deposit Tokens
         require(
             token1.transferFrom(msg.sender, address(this), _token1Amount),
-            "failed to transfer token1"
+            "failed to transfer token 1"
         );
-
         require(
             token2.transferFrom(msg.sender, address(this), _token2Amount),
-            "failed to transfer token2"
-        );       
+            "failed to transfer token 2"
+        );
 
         // Issue Shares
-        uint256 share; 
+        uint256 share;
 
-        // If adding liquidity for the first time make share 100
+        // If first time adding liquidity, make share 100
         if (totalShares == 0) {
             share = 100 * PRECISION;
         } else {
             uint256 share1 = (totalShares * _token1Amount) / token1Balance;
             uint256 share2 = (totalShares * _token2Amount) / token2Balance;
             require(
-                (share1 / 10**3) == (share2 / 10**3), 
-                "Must provide equal token amounts"
+                (share1 / 10**3) == (share2 / 10**3),
+                "must provide equal token amounts"
             );
-
-            share = share1; 
+            share = share1;
         }
 
         // Manage Pool
         token1Balance += _token1Amount;
         token2Balance += _token2Amount;
-        K = token1Balance * token2Balance; 
+        K = token1Balance * token2Balance;
 
-        // Update shares
+        // Updates shares
         totalShares += share;
         shares[msg.sender] += share;
     }
 
-    //Determine amount of token2 need when depositing liquidity for token 1
-    function calculateToken2Deposit(uint256 _token1Amount) 
-        public 
-        view 
-        returns(uint256 token2Amount) 
+    // Determine how many token2 tokens must be deposited when depositing liquidity for token1
+    function calculateToken2Deposit(uint256 _token1Amount)
+        public
+        view
+        returns (uint256 token2Amount)
     {
-        token2Amount = (token2Balance * _token1Amount) / token1Balance; 
+        token2Amount = (token2Balance * _token1Amount) / token1Balance;
     }
 
-    //Determine amount of token1 need when depositing liquidity for token 2
-    function calculateToken1Deposit(uint256 _token2Amount) 
-        public 
-        view 
-        returns(uint256 token1Amount) 
+    // Determine how many token1 tokens must be deposited when depositing liquidity for token2
+    function calculateToken1Deposit(uint256 _token2Amount)
+        public
+        view
+        returns (uint256 token1Amount)
     {
-        token1Amount = (token1Balance * _token2Amount) / token2Balance; 
+        token1Amount = (token1Balance * _token2Amount) / token2Balance;
     }
 
     // Returns amount of token2 received when swapping token1
@@ -101,33 +93,29 @@ contract AMM {
         view
         returns (uint256 token2Amount)
     {
-        uint256 token1After = token1Balance +_token1Amount;
+        uint256 token1After = token1Balance + _token1Amount;
         uint256 token2After = K / token1After;
         token2Amount = token2Balance - token2After;
 
-        // Don't let pool go to 0
+        // Don't let the pool go to 0
         if (token2Amount == token2Balance) {
             token2Amount--;
         }
 
-        require(token2Amount < token2Balance, "swap amount is to big");
+        require(token2Amount < token2Balance, "swap amount to large");
     }
 
-    function swapToken1(uint256 _token1Amount) 
-        external 
+    function swapToken1(uint256 _token1Amount)
+        external
         returns(uint256 token2Amount)
-    { 
-        //Calculate Token 2 Amount
+    {
+        // Calculate Token 2 Amount
         token2Amount = calculateToken1Swap(_token1Amount);
 
         // Do Swap
-        // 1. Transfer token1 tokens out of user wallet to cotract
         token1.transferFrom(msg.sender, address(this), _token1Amount);
-        // 2. Update the token1 balance in the contract
         token1Balance += _token1Amount;
-        // 3. Update the token2 balance in the contract
-        token1Balance -= token2Amount;
-        // 4. Transfer token2 tokens from cotract to user wallet 
+        token2Balance -= token2Amount;
         token2.transfer(msg.sender, token2Amount);
 
         // Emit an event
@@ -144,38 +132,34 @@ contract AMM {
     }
 
     // Returns amount of token1 received when swapping token2
-    function calculateToken2Swap(uint256 _token2mount)
+    function calculateToken2Swap(uint256 _token2Amount)
         public
         view
         returns (uint256 token1Amount)
     {
-        uint256 token2After = token2Balance + _token2mount;
+        uint256 token2After = token2Balance + _token2Amount;
         uint256 token1After = K / token2After;
         token1Amount = token1Balance - token1After;
 
-        // Don't let pool go to 0
+        // Don't let the pool go to 0
         if (token1Amount == token1Balance) {
             token1Amount--;
         }
 
-        require(token1Amount < token1Balance, "swap amount is to big");
+        require(token1Amount < token1Balance, "swap amount to large");
     }
 
-    function swapToken2(uint256 _token2Amount) 
-        external 
+    function swapToken2(uint256 _token2Amount)
+        external
         returns(uint256 token1Amount)
-    { 
-        //Calculate Token 1 Amount
+    {
+        // Calculate Token 1 Amount
         token1Amount = calculateToken2Swap(_token2Amount);
 
         // Do Swap
-        // 1. Transfer token2 tokens out of user wallet to cotract
         token2.transferFrom(msg.sender, address(this), _token2Amount);
-        // 2. Update the token2 balance in the contract
         token2Balance += _token2Amount;
-        // 3. Update the token1 balance in the contract
         token1Balance -= token1Amount;
-        // 4. Transfer token1 tokens from cotract to user wallet 
         token1.transfer(msg.sender, token1Amount);
 
         // Emit an event
@@ -185,11 +169,43 @@ contract AMM {
             _token2Amount,
             address(token1),
             token1Amount,
-            token2Balance,
             token1Balance,
+            token2Balance,
             block.timestamp
         );
     }
 
-}
+    // Determine how many tokens will be withdrawn
+    function calculateWithdrawAmount(uint256 _share)
+        public
+        view
+        returns (uint256 token1Amount, uint256 token2Amount)
+    {
+        require(_share <= totalShares, "must be less than total shares");
+        token1Amount = (_share * token1Balance) / totalShares;
+        token2Amount = (_share * token2Balance) / totalShares;
+    }
 
+    // Removes liquidity from the pool
+    function removeLiquidity(uint256 _share)
+        external
+        returns(uint256 token1Amount, uint256 token2Amount)
+    {
+        require(
+            _share <= shares[msg.sender],
+            "cannot withdraw more shares than you have"
+        );
+
+        (token1Amount, token2Amount) = calculateWithdrawAmount(_share);
+
+        shares[msg.sender] -= _share;
+        totalShares -= _share;
+
+        token1Balance -= token1Amount;
+        token2Balance -= token2Amount;
+        K = token1Balance * token2Balance;
+
+        token1.transfer(msg.sender, token1Amount);
+        token2.transfer(msg.sender, token2Amount);
+    }
+}
