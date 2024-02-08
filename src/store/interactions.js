51ab0daf080c5b828c0dde8b626096e44e49bc1a
henryvishnevsky <github.hn9cy@passmail.com>
2024-02-07 22:02:25 -0700

    Add Deposit component

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -5,7 +5,10 @@ import { setContracts, setSymbols, balancesLoaded } from './reducers/tokens'
 
 import { 
 	setContract, 
-	sharesLoaded, 
+	sharesLoaded,
+	depositRequest,
+  depositSuccess,
+  depositFail, 
 	swapRequest,
 	swapSuccess,
   swapFail
@@ -76,6 +79,31 @@ export const loadBalances = async (amm, tokens, account, dispatch) => {
   const shares = await amm.shares(account)
   dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
 
+}
+
+// ------------------------------------------------------------------------------
+// ADD LIQUDITY
+export const addLiquidity = async (provider, amm, tokens, amounts, dispatch) => {
+  try {
+    dispatch(depositRequest())
+
+    const signer = await provider.getSigner()
+
+    let transaction
+
+    transaction = await tokens[0].connect(signer).approve(amm.address, amounts[0])
+    await transaction.wait()
+
+    transaction = await tokens[1].connect(signer).approve(amm.address, amounts[1])
+    await transaction.wait()
+
+    transaction = await amm.connect(signer).addLiquidity(amounts[0], amounts[1])
+    await transaction.wait()
+
+    dispatch(depositSuccess(transaction.hash))
+  } catch (error) {
+    dispatch(depositFail())
+  }
 }
 
 
